a70fd48a8697f6c29f5283a72d14ceaf661473a1
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2020-08-26 21:14:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/codingfaction/TestActivity.java
@@ -0,0 +1,33 @@
+package com.example.codingfaction;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        Toolbar toolbar = findViewById(R.id.test_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Test");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
